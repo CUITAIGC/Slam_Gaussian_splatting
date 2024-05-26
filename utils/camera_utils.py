b7039bcd9fc35aaa650222a5347a837cc200279d
guowenwu <1817919842@qq.com>
2024-05-26 15:33:30 +0800

    init monoFastGS

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -1,82 +1,154 @@
-#
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use 
-# under the terms of the LICENSE.md file.
-#
-# For inquiries contact  george.drettakis@inria.fr
-#
+import torch
+from torch import nn
 
-from scene.cameras import Camera
-import numpy as np
-from utils.general_utils import PILtoTorch
-from utils.graphics_utils import fov2focal
+from gaussian_splatting.utils.graphics_utils import getProjectionMatrix2, getWorld2View2
+from utils.slam_utils import image_gradient, image_gradient_mask
 
-WARNED = False
 
-def loadCam(args, id, cam_info, resolution_scale):
-    orig_w, orig_h = cam_info.image.size
+class Camera(nn.Module):
+    def __init__(
+        self,
+        uid,
+        color,
+        depth,
+        gt_T,
+        projection_matrix,
+        fx,
+        fy,
+        cx,
+        cy,
+        fovx,
+        fovy,
+        image_height,
+        image_width,
+        device="cuda:0",
+    ):
+        super(Camera, self).__init__()
+        self.uid = uid
+        self.device = device
 
-    if args.resolution in [1, 2, 4, 8]:
-        resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
-    else:  # should be a type that converts to float
-        if args.resolution == -1:
-            if orig_w > 1600:
-                global WARNED
-                if not WARNED:
-                    print("[ INFO ] Encountered quite large input images (>1.6K pixels width), rescaling to 1.6K.\n "
-                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
-                    WARNED = True
-                global_down = orig_w / 1600
-            else:
-                global_down = 1
+        T = torch.eye(4, device=device)
+        self.R = T[:3, :3]
+        self.T = T[:3, 3]
+        self.R_gt = gt_T[:3, :3]
+        self.T_gt = gt_T[:3, 3]
+
+        self.original_image = color
+        self.depth = depth
+        self.grad_mask = None
+
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
+        self.FoVx = fovx
+        self.FoVy = fovy
+        self.image_height = image_height
+        self.image_width = image_width
+
+        self.cam_rot_delta = nn.Parameter(
+            torch.zeros(3, requires_grad=True, device=device)
+        )
+        self.cam_trans_delta = nn.Parameter(
+            torch.zeros(3, requires_grad=True, device=device)
+        )
+
+        self.exposure_a = nn.Parameter(
+            torch.tensor([0.0], requires_grad=True, device=device)
+        )
+        self.exposure_b = nn.Parameter(
+            torch.tensor([0.0], requires_grad=True, device=device)
+        )
+
+        self.projection_matrix = projection_matrix.to(device=device)
+
+    @staticmethod
+    def init_from_dataset(dataset, idx, projection_matrix):
+        gt_color, gt_depth, gt_pose = dataset[idx]
+        return Camera(
+            idx,
+            gt_color,
+            gt_depth,
+            gt_pose,
+            projection_matrix,
+            dataset.fx,
+            dataset.fy,
+            dataset.cx,
+            dataset.cy,
+            dataset.fovx,
+            dataset.fovy,
+            dataset.height,
+            dataset.width,
+            device=dataset.device,
+        )
+
+    @staticmethod
+    def init_from_gui(uid, T, FoVx, FoVy, fx, fy, cx, cy, H, W):
+        projection_matrix = getProjectionMatrix2(
+            znear=0.01, zfar=100.0, fx=fx, fy=fy, cx=cx, cy=cy, W=W, H=H
+        ).transpose(0, 1)
+        return Camera(
+            uid, None, None, T, projection_matrix, fx, fy, cx, cy, FoVx, FoVy, H, W
+        )
+
+    @property
+    def world_view_transform(self):
+        return getWorld2View2(self.R, self.T).transpose(0, 1)
+
+    @property
+    def full_proj_transform(self):
+        return (
+            self.world_view_transform.unsqueeze(0).bmm(
+                self.projection_matrix.unsqueeze(0)
+            )
+        ).squeeze(0)
+
+    @property
+    def camera_center(self):
+        return self.world_view_transform.inverse()[3, :3]
+
+    def update_RT(self, R, t):
+        self.R = R.to(device=self.device)
+        self.T = t.to(device=self.device)
+
+    def compute_grad_mask(self, config):
+        edge_threshold = config["Training"]["edge_threshold"]
+
+        gray_img = self.original_image.mean(dim=0, keepdim=True)
+        gray_grad_v, gray_grad_h = image_gradient(gray_img)
+        mask_v, mask_h = image_gradient_mask(gray_img)
+        gray_grad_v = gray_grad_v * mask_v
+        gray_grad_h = gray_grad_h * mask_h
+        img_grad_intensity = torch.sqrt(gray_grad_v**2 + gray_grad_h**2)
+
+        if config["Dataset"]["type"] == "replica":
+            row, col = 32, 32
+            multiplier = edge_threshold
+            _, h, w = self.original_image.shape
+            for r in range(row):
+                for c in range(col):
+                    block = img_grad_intensity[
+                        :,
+                        r * int(h / row) : (r + 1) * int(h / row),
+                        c * int(w / col) : (c + 1) * int(w / col),
+                    ]
+                    th_median = block.median()
+                    block[block > (th_median * multiplier)] = 1
+                    block[block <= (th_median * multiplier)] = 0
+            self.grad_mask = img_grad_intensity
         else:
-            global_down = orig_w / args.resolution
+            median_img_grad_intensity = img_grad_intensity.median()
+            self.grad_mask = (
+                img_grad_intensity > median_img_grad_intensity * edge_threshold
+            )
 
-        scale = float(global_down) * float(resolution_scale)
-        resolution = (int(orig_w / scale), int(orig_h / scale))
+    def clean(self):
+        self.original_image = None
+        self.depth = None
+        self.grad_mask = None
 
-    resized_image_rgb = PILtoTorch(cam_info.image, resolution)
+        self.cam_rot_delta = None
+        self.cam_trans_delta = None
 
-    gt_image = resized_image_rgb[:3, ...]
-    loaded_mask = None
-
-    if resized_image_rgb.shape[1] == 4:
-        loaded_mask = resized_image_rgb[3:4, ...]
-
-    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
-                  FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
-                  image=gt_image, gt_alpha_mask=loaded_mask,
-                  image_name=cam_info.image_name, uid=id, data_device=args.data_device)
-
-def cameraList_from_camInfos(cam_infos, resolution_scale, args):
-    camera_list = []
-
-    for id, c in enumerate(cam_infos):
-        camera_list.append(loadCam(args, id, c, resolution_scale))
-
-    return camera_list
-
-def camera_to_JSON(id, camera : Camera):
-    Rt = np.zeros((4, 4))
-    Rt[:3, :3] = camera.R.transpose()
-    Rt[:3, 3] = camera.T
-    Rt[3, 3] = 1.0
-
-    W2C = np.linalg.inv(Rt)
-    pos = W2C[:3, 3]
-    rot = W2C[:3, :3]
-    serializable_array_2d = [x.tolist() for x in rot]
-    camera_entry = {
-        'id' : id,
-        'img_name' : camera.image_name,
-        'width' : camera.width,
-        'height' : camera.height,
-        'position': pos.tolist(),
-        'rotation': serializable_array_2d,
-        'fy' : fov2focal(camera.FovY, camera.height),
-        'fx' : fov2focal(camera.FovX, camera.width)
-    }
-    return camera_entry
+        self.exposure_a = None
+        self.exposure_b = None
